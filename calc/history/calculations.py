8e7bcf754399edf4b3421f42823f877de357fcb4
NikitaGaikwad01 <ng46@njit.edu>
2021-11-18 12:41:06 -0500

    part4 pylint refactor

---
+++ calc/history/calculations.py
@@ -0,0 +1,28 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+"""Calculation history Class"""
+class Calculations:
+    history = []
+    # pylint: disable=too-few-public-methods
+    @staticmethod
+    def clear_history():
+        Calculations.history.clear()
+        return True
+    @staticmethod
+    def count_history():
+        return len(Calculations.history)
+    @staticmethod
+    def get_last_calculation():
+        return Calculations.history[-1]
+    @staticmethod
+    def get_first_calculation():
+        return Calculations.history[-1]
+    @staticmethod
+    def get_calculation(num):
+        """ get a specific calculation from history"""
+        return Calculations.history[num]
+    @staticmethod
+    def add_calculation(calculation):
+        """ get a specific calculation from history"""
+        return Calculations.history.append(calculation)
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,46 +1,24 @@
-"""Testing the Calculator Program"""
-import pprint
+"""Testing the Calculator"""
 import pytest
-from calculator.calculator import Calculator
-
-# Defining the Fixture
+from calc.calculator import Calculator
+from calc.history.calculations import Calculations
 @pytest.fixture
-def clear_history():
-    """Calling the clear history Method"""
-    Calculator.clear_history()
+def clear_history_fixture():
+    """define a function that will run each time you pass it to a test, it is called a fixture"""
+    # pylint: disable=redefined-outer-name
+    Calculations.clear_history()
+#You have to add the fixture function as a parameter to the test that you want to use it with
+def test_calculator_add_static(clear_history_fixture):
+    """testing that our calculator has a static method for addition"""
+    # pylint: disable=unused-argument,redefined-outer-name
+    assert Calculator.add_numbers(1.0,2.0,3.0) == 6.0
 
-# pylint: disable=unused-argument, redefined-outer-name
-def test_calculator_addition(clear_history):
-    """Testing the Add Method for our calculator class"""
-    assert Calculator.addition_nums(5, 5) == 10
-    assert Calculator.addition_nums(20, 30) == 50
-    assert Calculator.addition_nums(3, 2) == 5
-    assert Calculator.addition_nums(4, 2) == 6
-    assert Calculator.addition_nums(50, 5) == 55
-    assert Calculator.get_calculation_count() == 5
-    assert Calculator.get_first_calculation_history() == 10
-    pprint.pprint(Calculator.history)
+def test_calculator_subtract_static(clear_history_fixture):
+    """Testing the subtract method of the calc"""
+    # pylint: disable=unused-argument,redefined-outer-name
+    assert Calculator.subtract_numbers(1.0,2.0) == -3.0
 
-# pylint: disable=unused-argument, redefined-outer-name
-def test_calculator_subtraction(clear_history):
-    """Testing the subtraction Method for our calculator class"""
-    assert Calculator.subtraction_nums(4,3) == 1
-    assert Calculator.subtraction_nums(50,20) == 30
-    assert Calculator.get_calculation_count() == 2
-    pprint.pprint(Calculator.history)
-
-# pylint: disable=unused-argument, redefined-outer-name
-def test_calculator_multiplication(clear_history):
-    """Testing the multiplication method for calculator class"""
-    assert Calculator.multiplication_nums(3,2) == 6
-    assert Calculator.multiplication_nums(10,20) == 200
-    assert Calculator.multiplication_nums(4,4) == 16
-    assert Calculator.get_calculation_count() == 3
-    pprint.pprint(Calculator.history)
-
-# pylint: disable=unused-argument, redefined-outer-name
-def test_calculator_division(clear_history):
-    """Testing the division method for calculator class"""
-    assert Calculator.division_nums(10, 5) == 2
-    assert Calculator.get_calculation_count() == 1
-    pprint.pprint(Calculator.history)
+def test_calculator_multiply_static(clear_history_fixture):
+    """Testing the subtract method of the calc"""
+    # pylint: disable=unused-argument,redefined-outer-name
+    assert Calculator.multiply_numbers(1.0,2.0) == 2.0
