1a4dd0dd6c7c4e1f14f1dd764b42d8d38a688512
NikitaGaikwad01 <ng46@njit.edu>
2021-12-15 18:30:48 -0500

    webapp flash message

--- app/controllers/calculator_controller.py
+++ app/controllers/calculator_controller.py
@@ -6,34 +6,37 @@ from flask import render_template, request, flash, redirect, url_for
 class CalculatorController(ControllerBase):
     @staticmethod
     def post():
+
+        # get the values out of the form
         value1 = request.form['value1']
         value2 = request.form['value2']
         operation = request.form['operation']
 
-        if request.form['value1'] == '':
-            error = ' Please enter valid input for Value 1 field  '
-
+        if request.form['value1'] == '' and request.form['value2'] == '':
+            flash('Please enter valid input in both the fields')
+        elif request.form['value1'] == '':
+            flash('Please enter valid input for Value 1 field')
         elif request.form['value2'] == '':
-            error = ' Please enter valid input for Value 2 field  '
-
-        elif request.form['value1'] == '' or request.form['value2'] == '':
-            error = '  Please enter valid input in both the fields '
-
+            flash('Please enter valid input for Value 2 field')
         else:
-            flash(' Calculation is done successfully ')
+            flash('Calculation is done successfully !!!!')
 
-            # get the values out of the form
-            my_tuple = (value1, value2)
-           # value1 = request.form['value1']
-            #value2 = request.form['value2']
-            #operation = request.form['operation']
             # make the tuple
-            #my_tuple = (value1, value2)
+            my_tuple = (value1, value2)
             # this will call the correct operation
             getattr(Calculator, operation)(my_tuple)
             result = str(Calculator.get_last_value())
-            return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result)
-        return render_template('calculator.html', error=error)
+
+            # write to file
+            str1 = value1 + "," + value2 + "," + operation + "," + result + '\n'
+            with open('output.txt', 'a+') as f:
+                f.write(str1)
+
+            with open('output.txt', 'r') as fr:
+                hist = fr.read().strip()
+
+            return render_template('result.html', val1=value1, val2=value2, ope=operation, res=result, hist=hist)
+        return render_template('calculator.html')
 
     @staticmethod
     def get():
